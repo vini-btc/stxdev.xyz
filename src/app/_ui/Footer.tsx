9204441d76fd3b5052273743a75cc461f21e0c18
Vini Andrade <me@vicnicius.com>
2024-06-08 17:28:32 +0200

    Improve Footer layout
    Signed-off-by: Vini Andrade <me@vicnicius.com>

--- src/app/_ui/Footer.tsx
+++ src/app/_ui/Footer.tsx
@@ -1,5 +1,19 @@
+import { GitHubLogoIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+
 export const Footer = () => {
   return (
-    <footer className="flex items-center text-sm">stxdev.xyz - copyleft</footer>
+    <footer className="flex items-center justify-between text-sm border-t border-white border-dotted pt-2 mt-2">
+      stxdev.xyz
+      <div className="flex space-x-2">
+        <Link href="https://github.com/vicnicius/stxdev.xyz">
+          <GitHubLogoIcon />
+        </Link>
+
+        <Link href="https://x.com/vicnicius">
+          <TwitterLogoIcon />
+        </Link>
+      </div>
+    </footer>
   );
 };
